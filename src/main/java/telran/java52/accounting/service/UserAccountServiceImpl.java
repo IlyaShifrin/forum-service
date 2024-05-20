f0d28e8042b6e4ad3d87748f9f8d531395e95a9f
Ilya <drunktrumpet1@gmail.com>
2024-05-20 18:59:23 +0300

    Added BCrypt

--- src/main/java/telran/java52/accounting/service/UserAccountServiceImpl.java
+++ src/main/java/telran/java52/accounting/service/UserAccountServiceImpl.java
@@ -1,5 +1,6 @@
 package telran.java52.accounting.service;
 
+import org.mindrot.jbcrypt.BCrypt;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
@@ -9,6 +10,8 @@ import telran.java52.accounting.dto.RolesDto;
 import telran.java52.accounting.dto.UserDto;
 import telran.java52.accounting.dto.UserEditDto;
 import telran.java52.accounting.dto.UserRegisterDto;
+import telran.java52.accounting.dto.exception.IncorrectRoleException;
+import telran.java52.accounting.dto.exception.UserExistsException;
 import telran.java52.accounting.dto.exception.UserNotFoundException;
 import telran.java52.accounting.model.UserAccount;
 
@@ -17,17 +20,19 @@ import telran.java52.accounting.model.UserAccount;
 public class UserAccountServiceImpl implements UserAccountService {
 
 	final ModelMapper modelMapper;
-	final UserAccountRepository userAccountRepository; 
-	
+	final UserAccountRepository userAccountRepository;
+
 	@Override
 	public UserDto register(UserRegisterDto userRegisterDto) {
 		if (userAccountRepository.existsById(userRegisterDto.getLogin())) {
-			return null;
+			throw new UserExistsException();
+			// return null;
 		}
-		
+
 		UserAccount user = modelMapper.map(userRegisterDto, UserAccount.class);
+		String password = BCrypt.hashpw(userRegisterDto.getPassword(), BCrypt.gensalt());
+		user.setPassword(password);
 		userAccountRepository.save(user);
-		
 		return modelMapper.map(user, UserDto.class);
 	}
 
@@ -47,40 +52,47 @@ public class UserAccountServiceImpl implements UserAccountService {
 	@Override
 	public UserDto updateUser(String login, UserEditDto userEditDto) {
 		UserAccount user = userAccountRepository.findById(login).orElseThrow(UserNotFoundException::new);
-		
+
 		String firstName = userEditDto.getFirstName();
 		if (firstName != null) {
 			user.setFirstName(firstName);
 		}
-		
+
 		String lastName = userEditDto.getLastName();
 		if (lastName != null) {
 			user.setLastName(lastName);
 		}
-		
+
 		userAccountRepository.save(user);
-		
 		return modelMapper.map(user, UserDto.class);
 	}
 
 	@Override
 	public RolesDto changeRolesList(String login, String role, boolean isAddRole) {
 		UserAccount user = userAccountRepository.findById(login).orElseThrow(UserNotFoundException::new);
-		
-		if (isAddRole) {
-			user.addRole(role);
-		} else {
-			user.removeRole(role);
+		boolean res;
+		try {
+			if (isAddRole) {
+				res = user.addRole(role);
+			} else {
+				res = user.removeRole(role);
+			}
+		} catch (Exception e) {
+			throw new IncorrectRoleException();
 		}
-		userAccountRepository.save(user);
-		
+
+		if (res) {
+			userAccountRepository.save(user);
+		}
+
 		return modelMapper.map(user, RolesDto.class);
 	}
 
 	@Override
 	public void changePassword(String login, String newPassword) {
 		UserAccount user = userAccountRepository.findById(login).orElseThrow(UserNotFoundException::new);
-		user.setPassword(newPassword);
+		String password = BCrypt.hashpw(newPassword, BCrypt.gensalt());
+		user.setPassword(password);
 		userAccountRepository.save(user);
 	}
 
